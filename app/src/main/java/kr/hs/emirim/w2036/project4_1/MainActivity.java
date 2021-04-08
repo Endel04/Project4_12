f065af3b8be972948c09ec24b5c203f9c9c6f5bc
유하람 <w2036@e-mirim.hs.kr>
2021-04-08 19:23:27 +0900

    Update Add Mod Button

--- app/src/main/java/kr/hs/emirim/w2036/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2036/project4_1/MainActivity.java
@@ -1,7 +1,6 @@
 package kr.hs.emirim.w2036.project4_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,25 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnPlus = findViewById(R.id.btn1);
         Button btnMinus = findViewById(R.id.btn2);
-        Button btnMulti = findViewById(R.id.btn3);
+        Button btnMult = findViewById(R.id.btn3);
         Button btnDiv = findViewById(R.id.btn4);
+        Button btnMod = findViewById(R.id.btn5);
         edit1 = findViewById(R.id.edit1);
-        edit1 = findViewById(R.id.edit2);
+        edit2 = findViewById(R.id.edit2);
         textResult = findViewById(R.id.t1);
 
         btnPlus.setOnClickListener(btnListener);
         btnMinus.setOnClickListener(btnListener);
-        btnMulti.setOnClickListener(btnListener);
+        btnMult.setOnClickListener(btnListener);
         btnDiv.setOnClickListener(btnListener);
     }
-    View.onClickListener btnListener = new View.OnClickListener(){
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
-        public void onClick(View v){
+        public void onClick(View v) {
             String strEdit1 = edit1.getText().toString();
             String strEdit2 = edit2.getText().toString();
             int num1 = Integer.parseInt(strEdit1);
             int num2 = Integer.parseInt(strEdit2);
             int result = 0;
+
             switch(v.getId()){
                 case R.id.btn1:
                     result = num1 + num2;
@@ -52,9 +54,13 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn4:
                     result = num1 / num2;
                     break;
+                case R.id.btn5:
+                    result = num1 % num2;
+                    break;
             }
             textResult.setText(R.string.t1);
             textResult.append(result + "");
         }
     };
+
 }
